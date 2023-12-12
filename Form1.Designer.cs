22211f12b0c58d25ad4b65f919016ec94f9664a5
SashaCX75 <g.sasha.13@gmail.com>
2023-12-12 18:12:16 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CreateZPK_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Create_Zpk = new System.Windows.Forms.Button();
+            this.radioButton_WinRar = new System.Windows.Forms.RadioButton();
+            this.radioButton_7Zip = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button_Create_Zpk
+            // 
+            this.button_Create_Zpk.Enabled = false;
+            this.button_Create_Zpk.Location = new System.Drawing.Point(12, 12);
+            this.button_Create_Zpk.Name = "button_Create_Zpk";
+            this.button_Create_Zpk.Size = new System.Drawing.Size(173, 23);
+            this.button_Create_Zpk.TabIndex = 0;
+            this.button_Create_Zpk.Text = "Create *.zpk file";
+            this.button_Create_Zpk.UseVisualStyleBackColor = true;
+            this.button_Create_Zpk.Click += new System.EventHandler(this.button_Create_Zpk_Click);
+            // 
+            // radioButton_WinRar
+            // 
+            this.radioButton_WinRar.AutoSize = true;
+            this.radioButton_WinRar.Enabled = false;
+            this.radioButton_WinRar.Location = new System.Drawing.Point(13, 42);
+            this.radioButton_WinRar.Name = "radioButton_WinRar";
+            this.radioButton_WinRar.Size = new System.Drawing.Size(61, 17);
+            this.radioButton_WinRar.TabIndex = 1;
+            this.radioButton_WinRar.TabStop = true;
+            this.radioButton_WinRar.Text = "WinRar";
+            this.radioButton_WinRar.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_7Zip
+            // 
+            this.radioButton_7Zip.AutoSize = true;
+            this.radioButton_7Zip.Enabled = false;
+            this.radioButton_7Zip.Location = new System.Drawing.Point(124, 41);
+            this.radioButton_7Zip.Name = "radioButton_7Zip";
+            this.radioButton_7Zip.Size = new System.Drawing.Size(49, 17);
+            this.radioButton_7Zip.TabIndex = 2;
+            this.radioButton_7Zip.TabStop = true;
+            this.radioButton_7Zip.Text = "7-Zip";
+            this.radioButton_7Zip.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 64);
+            this.Controls.Add(this.radioButton_7Zip);
+            this.Controls.Add(this.radioButton_WinRar);
+            this.Controls.Add(this.button_Create_Zpk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create *.zpk file";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Create_Zpk;
+        private System.Windows.Forms.RadioButton radioButton_WinRar;
+        private System.Windows.Forms.RadioButton radioButton_7Zip;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateZPK_2
+{
+    public partial class Form1 : Form
+    {
+        string _WinRAR = @"C:\Program Files\WinRAR\WinRAR.exe";
+        string _7zPath = @"C:\Program Files\7-Zip\7z.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Create_Zpk_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Watch faces (*.zip) | *.zip";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string zpkFileName = CreateZPK(openFileDialog.FileName);
+                if (zpkFileName != null && zpkFileName.Length > 0)
+                {
+                    // открываем файл если создали его
+                    if (File.Exists(zpkFileName))
+                    {
+                        Process.Start(new ProcessStartInfo("explorer.exe", " /select, " + zpkFileName));
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(_7zPath))
+            {
+                radioButton_7Zip.Enabled = true;
+                radioButton_7Zip.Checked = true;
+                button_Create_Zpk.Enabled = true;
+            }
+            if (File.Exists(_WinRAR))
+            {
+                radioButton_WinRar.Enabled = true;
+                radioButton_WinRar.Checked = true;
+                button_Create_Zpk.Enabled = true;
+            }
+        }
+
+        private string CreateZPK(string fullFileName)
+        {
+            if (!radioButton_WinRar.Checked && !radioButton_7Zip.Checked) return null;
+            if (!File.Exists(fullFileName)) return null;
+            string tempDir = Path.Combine(Application.StartupPath, "Temp");
+            string targetDir = Path.GetDirectoryName(fullFileName);
+            string fileName = Path.GetFileNameWithoutExtension(fullFileName);
+            string zipFileName = Path.Combine(tempDir, "device.zip");
+            string zpkFileName = Path.Combine(targetDir, fileName + ".zpk");
+
+            if (Directory.Exists(tempDir)) DeleteDirectory(tempDir);
+            ZipArchive zip = System.IO.Compression.ZipFile.OpenRead(fullFileName);
+            List<ZipArchiveEntry> fileList = zip.Entries.ToList();
+            if (!IsWatchFace(fileList))
+            {
+                MessageBox.Show("Perhaps the file is not a watch face.");
+                return null;
+            }
+            if (File.Exists(zpkFileName))
+            {
+                DialogResult result = MessageBox.Show("Such a zpk file already exists. Should I overwrite it?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Cancel) return null;
+                File.Delete(zpkFileName);
+            }
+
+            if (!Directory.Exists(tempDir)) Directory.CreateDirectory(tempDir);
+            File.WriteAllBytes(zpkFileName, Properties.Resources.WatchFace);
+            File.Copy(fullFileName, zipFileName, true);
+
+            if (radioButton_WinRar.Checked)
+            {
+                string arguments = "a -ep \"";
+                arguments += zpkFileName;
+                arguments += "\" \"";
+                arguments += zipFileName;
+                arguments += "\"";
+
+                //Process.Start(_WinRAR, arguments);
+                using (Process pr = new Process())
+                {
+                    pr.StartInfo.FileName = _WinRAR;
+                    pr.StartInfo.Arguments = arguments;
+                    pr.StartInfo.UseShellExecute = false;
+                    pr.Start();
+                    pr.WaitForExit();
+                } 
+            }
+            else if (radioButton_7Zip.Checked)
+            {
+                string arguments = "a \"";
+                arguments += zpkFileName;
+                arguments += "\" \"";
+                arguments += zipFileName;
+                arguments += "\"";
+
+                using (Process pr = new Process())
+                {
+                    pr.StartInfo.FileName = _7zPath;
+                    pr.StartInfo.Arguments = arguments;
+                    pr.StartInfo.UseShellExecute = false;
+                    pr.Start();
+                    pr.WaitForExit();
+                }
+            }
+
+            DeleteDirectory(tempDir);
+            return zpkFileName;
+        }
+
+        // Проверяем что есть необходимые файлы для циферблата
+        private bool IsWatchFace(List<ZipArchiveEntry> fileList)
+        {
+            bool appJs = false;
+            bool appJson = false;
+            bool assets = false;
+            foreach (ZipArchiveEntry item in fileList)
+            {
+                if (item.FullName == "app.js") appJs = true;
+                if (item.FullName == "app.bin") appJs = true;
+                if (item.FullName == "app.json") appJson = true;
+                if (item.FullName == "assets/") assets = true;
+                if (appJs && appJson && assets) return true;
+            }
+            return false;
+        }
+
+        public static void DeleteDirectory(string target_dir)
+        {
+            foreach (string file in Directory.GetFiles(target_dir))
+            {
+                File.Delete(file);
+            }
+
+            foreach (string subDir in Directory.GetDirectories(target_dir))
+            {
+                DeleteDirectory(subDir);
+            }
+
+            Thread.Sleep(1); // This makes the difference between whether it works or not. Sleep(0) is not enough.
+            Directory.Delete(target_dir);
+        }
+
+    }
+}
